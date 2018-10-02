1658b9a2c26d85f73bb7fce90c9d5e701087becb
Gonzalo Pincheira <gonzalo.pincheira@globant.com>
2018-10-02 00:50:38 -0300

    basic configuration and tests

---
+++ test/login.spec.js
@@ -0,0 +1,45 @@
+import { LoginForm } from '../src/login';
+
+describe('Login Form Component', () => {
+    const logComponent = (title = '') => {
+        console.log(`******************** ${title} ****************************`);
+        console.log(document.querySelector('.login-component__wrapper').innerHTML);
+        console.log('************************************************');
+    };
+    let loginForm;
+
+    beforeEach(() => {
+        loginForm = new LoginForm();
+    });
+    afterEach(() => {});
+
+    it('should be create an instance of LoginForm class', () => {
+        expect(loginForm instanceof LoginForm).toBe(true);
+    });
+
+    it('should set validation error when an input is not valid', () => {
+        const isValid = false;
+        const targetInput = document.querySelector('input#username');
+        const errorMessage = 'Please write an valid email address.';
+        
+        loginForm.setValidationError(isValid, targetInput, errorMessage);
+        
+        const errorWrapper = document.querySelector('#username ~ p');
+        expect(errorWrapper.textContent).toEqual(errorMessage);
+        expect(targetInput.classList.contains('validation-error')).toBe(true);
+    });
+
+    it('should remove validation error message when the input is valid', () => {
+        let isValid = false;
+        const targetInput = document.querySelector('input#username');
+        const errorMessage = 'Please write an valid email address.';
+        loginForm.setValidationError(isValid, targetInput, errorMessage);
+
+        isValid = true;
+        loginForm.setValidationError(isValid, targetInput, errorMessage);
+        
+        const errorWrapper = document.querySelector('#username ~ p');
+        expect(errorWrapper).toBe(null);
+        expect(targetInput.classList.contains('validation-error')).toBe(false);
+    });
+});
